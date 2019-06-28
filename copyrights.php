1dd2288d5d318671eb0d91a575dabf30bc5c7f7d
Isrnameo <52245732+Isrnameo@users.noreply.github.com>
2019-06-28 03:17:07 -0400

    Create copyrights.php

---
+++ copyrights.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+
+<div class="container w-75 p-5 mt-5">
+<h1>Copyright Notices</h1>
+<p>Ill add something more detailed later? I assume putting i have a zero fucks given polict wouldn't be great on a live server :) </p>
+</div>
+        
+
+
+<?php include 'footer.php'; ?>

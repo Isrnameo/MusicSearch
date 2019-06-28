2d7267d5c6c68f25ae87020aa7f349e23cae7bc8
Isrnameo <52245732+Isrnameo@users.noreply.github.com>
2019-06-28 01:41:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php
+<?php include 'header.php'; ?>
+
+<div class="container w-75 p-5 mt-5">
+    <h1 class="display-1 font-weight-bold text-secondary">Music Search Engine</h1>
+</div>
+
+<?php include 'isolated.php' ?>
+<?php include 'footer.php'; ?>

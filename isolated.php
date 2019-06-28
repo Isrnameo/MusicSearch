95c588837a480a403df8f7c13980426112906002
Isrnameo <52245732+Isrnameo@users.noreply.github.com>
2019-06-28 01:44:08 -0400

    Create isolated.php

---
+++ isolated.php
@@ -0,0 +1,10 @@
+<body>
+             <div class="container mb-5">
+                 
+                     <form action="music.php" method="get" class="my-3 input-group">
+                    <input class="form-control-lg" type="text" name="searching" size="80" placeholder="Free Music Awaits!">
+                    <button type="submit" name="search" class="btn btn-light btn-lg m-0" value="Search">Search</button>
+                 </form>
+                 
+             </div>
+<div class="container-fluid">

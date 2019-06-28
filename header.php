df307f5959d086393c00695c0246cb1ab1ec567a
Isrnameo <52245732+Isrnameo@users.noreply.github.com>
2019-06-28 00:38:47 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>MusicSearch</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    
+    <meta http-equiv="x-dns-prefetch-control" content="off">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    
+    <link rel="stylesheet" href="style.css">
+    
+    <link rel="canonical" href="" />
+    <link rel="author" href="humans.txt">
+    
+    <meta name="robots" content="index,follow">
+    
+</head>
+<body>
